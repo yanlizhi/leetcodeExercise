bc59c5495bda5505393c7d5888c4bca20a1d1667
Eric <17601320965@163.com>
2023-02-09 11:57:36 +0800

    twoSum

--- 1.两数之和.java
+++ 1.两数之和.java
@@ -7,7 +7,15 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
+        int length = nums.length;
+        for(int i = 0;i < length - 1;i++){
+            for(int j = i + 1;j < length;j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
     }
 }
 // @lc code=end
